d00499f59ea21b10e2931d92bc1f0c0dd4ea6bd3
Agustin Ferrari <agustin.ferrari@colaboradora.mx>
2016-04-22 23:32:31 -0500

    disabled compressor

--- carpool/carpool/settings/staging.py
+++ carpool/carpool/settings/staging.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-COMPRESS_ENABLED = True
+#COMPRESS_ENABLED = True
 
 ALLOWED_HOSTS = []
 
